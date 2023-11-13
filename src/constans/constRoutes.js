2932def704c2a8de9b4e130b0d058305f7fe30f4
My name <me@example.com>
2023-11-13 21:00:35 +0200

    First Commit

---
+++ src/constans/constRoutes.js
@@ -0,0 +1,13 @@
+export const ROUTES = {
+    BASE: '',
+    HOME: 'home',
+    LIBRARY: 'library',
+    CHAT: 'chat',
+    SIGNUP: 'signup',
+    LOGIN: 'login',
+    WORKSPACE: 'workspace',
+    JOINWORKSPACE: 'joinworkspace',
+    ACCESSWORKSPACE: 'accessworkspace',
+    SHARE: 'share',
+    EDITIMAGE: 'editimage',
+}
